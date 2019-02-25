347d525d2714101df2f1a1691f8511a37b88548e
Shahul Hameed. S <shahul.s@zohocorp.com>
2019-02-25 14:04:41 +0530

    updated buildscript for CI integration

--- test/cleanbuild_coverage.sh
+++ test/cleanbuild_coverage.sh
@@ -1,9 +1,9 @@
-clear
-rm -rf build/*
+cd test
+rm -rf build
+mkdir build
 cd build
 cmake ..
 # cmake -I "/mymac/code/sdk/zoho-iot-sdk-arduino/unit_test/sample/fakeit_master/include" -I"/mymac/code/sdk/zoho-iot-sdk-arduino/unit_test/sample/fakeit_master/config/catch" ..
 make unit_tests_coverage -j 8
 ./unit_tests
-cd ..
 
