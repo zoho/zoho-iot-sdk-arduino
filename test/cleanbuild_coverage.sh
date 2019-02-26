df64285ee10d99b1453d1e1b8e71a3ab0bc27772
Shahul Hameed. S <shahul.s@zohocorp.com>
2019-02-26 17:02:59 +0530

    code coverge generation is moved to shell script from Cmake

--- test/cleanbuild_coverage.sh
+++ test/cleanbuild_coverage.sh
@@ -2,9 +2,14 @@ cd test
 rm -rf build
 mkdir build
 cd build
-cmake ..
-# cmake -I "/mymac/code/sdk/zoho-iot-sdk-arduino/unit_test/sample/fakeit_master/include" -I"/mymac/code/sdk/zoho-iot-sdk-arduino/unit_test/sample/fakeit_master/config/catch" ..
-make unit_tests_coverage -j 8
-./unit_tests -r junit > unit_test_result.xml
-./unit_tests unit_tests_coverage
 
+cmake ..
+make -j 8
+
+./unit_tests -r junit > unit_test_result.xml
+./unit_tests
+
+lcov -c -d . -o coverage_swp.info
+lcov --REMOVE coverage_swp.info 'tests/*' '/usr/*' '/zoho-iot-sdk-arduino/test/*' --OUTPUT-FILE coverage.info
+
+genhtml coverage.info -o coverage
