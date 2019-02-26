148a972b94c98656667a2a0cc6288f4de382118a
Shahul Hameed. S <shahul.s@zohocorp.com>
2019-02-26 12:31:17 +0530

    unit test report and test coverage changes

--- test/cleanbuild_coverage.sh
+++ test/cleanbuild_coverage.sh
@@ -5,5 +5,6 @@ cd build
 cmake ..
 # cmake -I "/mymac/code/sdk/zoho-iot-sdk-arduino/unit_test/sample/fakeit_master/include" -I"/mymac/code/sdk/zoho-iot-sdk-arduino/unit_test/sample/fakeit_master/config/catch" ..
 make unit_tests_coverage -j 8
-./unit_tests
+./unit_tests -r junit > unit_test_result.xml
+./unit_tests unit_tests_coverage
 
